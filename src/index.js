b80045315fa7539376d9d496864b7e95b40467c4
yoon ji hoon <koom@utplus.co.kr>
2023-03-21 04:56:11 +0900

    test

--- src/index.js
+++ src/index.js
@@ -1,7 +1,27 @@
-import Koa from 'koa';
+const Koa = require('koa');
+const Router = require('@koa/router')
+const bodyParser = require('koa-bodyparser')
+
+
 const app = new Koa();
+const router = new Router();
+const cors = require('@koa/cors')
+
+app.use(cors());
+
+const tempData = {
+  users: ['ㅁㅁㅁㅁ', 'ㅁㄴㅇㄹ', 'ㅁㄴㅇㄹ'],
+  points: ['10','123123', '551']
+};
+
+
+
+router.get('/api/users', ctx => {
+  ctx.body = {ok: true, users: tempData.users}
+})
 
 const port = 9898;
+app.use(bodyParser()).use(router.routes());
 
 app.listen(port, () => {
   console.log('koomsday member server on');
